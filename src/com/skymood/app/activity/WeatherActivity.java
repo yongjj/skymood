1afff0a278d53d45f61afba1afc40b1f2cee5fc5
1325592345@qq.com <1325592345@qq.com>
2015-05-16 19:25:27 +0800

    xianshi

---
+++ src/com/skymood/app/activity/WeatherActivity.java
@@ -0,0 +1,121 @@
+package com.skymood.app.activity;
+
+import com.skymood.app.R;
+import com.skymood.app.util.HttpCallbackListener;
+import com.skymood.app.util.HttpUtil;
+import com.skymood.app.util.Utility;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class WeatherActivity extends Activity {
+	
+	private LinearLayout weatherInfoLayout;
+	private TextView cityNameText;
+	private TextView publishText;
+	private TextView weatherDespText;
+	private TextView temp1Text;
+	private TextView temp2Text;
+	private TextView currentDateText;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.weather_layout);
+		
+		weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+		cityNameText = (TextView) findViewById(R.id.city_name);
+		publishText = (TextView) findViewById(R.id.publish_text);
+		weatherDespText = (TextView) findViewById(R.id.weather_desp);
+		temp1Text = (TextView) findViewById(R.id.temp1);
+		temp2Text = (TextView) findViewById(R.id.temp2);
+		currentDateText = (TextView) findViewById(R.id.current_date);
+		
+		String countyCode = getIntent().getStringExtra("county_code");
+		
+		if (!TextUtils.isEmpty(countyCode)) {
+			publishText.setText("同步中...");
+			weatherInfoLayout.setVisibility(View.INVISIBLE);
+			cityNameText.setVisibility(View.INVISIBLE);
+			queryWeatherCode(countyCode);
+		} else {
+			showWeather();
+		}
+	}
+	
+	private void queryWeatherCode(String countyCode) {
+		String address = "http://www.weather.com.cn/data/list3/city" + countyCode + ".xml";
+		queryFromServer(address, "countyCode");
+	}
+	
+	private void queryWeatherInfo(String weatherCode) {
+		String address = "http://www.weather.com.cn/data/cityinfo/" + weatherCode + ".html";
+		queryFromServer(address, "weatherCode");
+	}
+	
+	private void queryFromServer(final String address, final String type) {
+		
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+
+			@Override
+			public void onFinish(final String response) {
+				if ("countyCode".equals(type)) {
+					String[] array = response.split("\\|");
+					if (array != null && array.length == 2) {
+						String weatherCode = array[1];
+						queryWeatherInfo(weatherCode);
+					}
+				} else if ("weatherCode".equals(type)) {
+					Utility.handleWeatherResponse(WeatherActivity.this, response);
+					
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							showWeather();
+						}
+					});
+				}
+				
+			}
+
+			@Override
+			public void onError(Exception e) {
+		
+				runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						publishText.setText("同步失败");
+					}
+				});
+				
+			}
+			
+		});
+	}
+	
+	private void showWeather() {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		cityNameText.setText(prefs.getString("city_name", ""));
+		temp1Text.setText(prefs.getString("temp1", ""));
+		temp2Text.setText(prefs.getString("temp2", ""));
+		weatherDespText.setText(prefs.getString("weather_desp", ""));
+		publishText.setText("今天" + prefs.getString("publish_time", "") + "发布");
+		currentDateText.setText(prefs.getString("current_date", ""));
+		weatherInfoLayout.setVisibility(View.VISIBLE);
+		cityNameText.setVisibility(View.VISIBLE);
+	}
+
+}
+
+
--- src/com/skymood/app/util/Utility.java
+++ src/com/skymood/app/util/Utility.java
@@ -1,5 +1,15 @@
 package com.skymood.app.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.skymood.app.db.City;
@@ -27,6 +37,7 @@ public class Utility {
 	
 	public synchronized static boolean handleCitiesResponse(SkyMoodDB skyMoodDB, 
 			String response, int provinceId) {
+		
 		if (!TextUtils.isEmpty(response)) {
 			String[] allCities = response.split(",");
 			if (allCities != null && allCities.length > 0) {
@@ -61,6 +72,43 @@ public class Utility {
 			}
 		}
 		return false;
-	}	
+	}
+	
+	public static void handleWeatherResponse(Context context, String response) {
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2, weatherDesp, publishTime);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveWeatherInfo(Context context, String cityName, String weatherCode, 
+			String temp1, String temp2, String weatherDesp, String publishTime) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyƒÍM‘¬d»’", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
+	}
 	
 }
+
+
+
+
+
